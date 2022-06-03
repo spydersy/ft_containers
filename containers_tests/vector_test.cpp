767735c0c3c4ecac0baeef5a5128e46a79c9bc55
spydersy <acbe1325@aol.com>
2022-06-03 19:07:25 +0100

    [IMPR] : iterator_traits struct

--- containers_tests/vector_test.cpp
+++ containers_tests/vector_test.cpp
@@ -1,7 +1,18 @@
 
 # include "../vector.hpp"
+
+# include "../utils/enable_if.hpp"
+# include "../utils/equal.hpp"
 # include "../utils/is_integral.hpp"
+# include "../utils/iterators_traits.hpp"
+# include "../utils/lexicographical_compare.hpp"
+
 
 int main() {
+  typedef ft::iterator_traits<int*> traits;
+
+  if (typeid(traits::iterator_category)==typeid(std::random_access_iterator_tag))
+    std::cout << "int* is a random-access iterator\n";
+
   return 0;
 }
--- utils/iterators_traits.hpp
+++ utils/iterators_traits.hpp
@@ -5,6 +5,46 @@
 namespace ft
 {
 
+    // Iterators Category :
+    struct  input_iterator_tag {};
+    struct  output_iterator_tag {};
+    struct  forward_iterator_tag       : public input_iterator_tag {};
+    struct  bidirectional_iterator_tag : public forward_iterator_tag {};
+    struct  random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+    // Iterators Traits : 
+    template <class Iterator>
+    class iterator_traits
+    {
+        // Member Types :
+        typedef  typename    Iterator::difference_type   difference_type;
+        typedef  typename    Iterator::value_type        value_type;
+        typedef  typename    Iterator::pointer           pointer;
+        typedef  typename    Iterator::reference         reference;
+        typedef  typename    Iterator::iterator_category iterator_category;
+    };
+
+    template <class T>
+    class iterator_traits<T*>
+    {
+        // Member Types :
+        typedef  ptrdiff_t   difference_type;
+        typedef  T        value_type;
+        typedef  T*           pointer;
+        typedef  T&         reference;
+        typedef  typename random_access_iterator_tag:: iterator_category;
+    };
+
+    template <class T>
+    class iterator_traits<const T*>
+    {
+        // Member Types :
+        typedef  typename    T::difference_type   difference_type;
+        typedef  typename    T::value_type        value_type;
+        typedef  typename    T::pointer           pointer;
+        typedef  typename    T::reference         reference;
+        typedef  typename    T::iterator_category iterator_category;
+    };
 };
 
 #endif
